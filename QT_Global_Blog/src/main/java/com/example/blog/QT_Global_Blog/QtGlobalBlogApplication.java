5f52a88d1ce15ec40c584713683764dfb996517c
Arsene Nyirinkwaya <dellamis11@gmail.com>
2024-08-01 00:20:58 +0200

    adding project to my git

---
+++ QT_Global_Blog/src/main/java/com/example/blog/QT_Global_Blog/QtGlobalBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog.QT_Global_Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtGlobalBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtGlobalBlogApplication.class, args);
+	}
+
+}
---
+++ QT_Global_Blog/src/test/java/com/example/blog/QT_Global_Blog/QtGlobalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog.QT_Global_Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtGlobalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
